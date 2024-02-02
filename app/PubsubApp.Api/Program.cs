e1073d7ea88397f673416eeba216ba9ce808112f
William <willsbctm@gmail.com>
2024-02-02 19:11:58 -0300

    Add worker and api

---
+++ app/PubsubApp.Api/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/outbox", () =>
+{
+    return Results.Ok();
+})
+.WithName("GetOutbox")
+.WithOpenApi();
+
+app.MapPost("/outbox", () =>
+{
+    return Results.Ok();
+})
+.WithName("PostOutbox")
+.WithOpenApi();
+
+app.Run();
+
---
+++ app/PubsubApp.Worker/Program.cs
@@ -0,0 +1,7 @@
+using PubsubApp.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
