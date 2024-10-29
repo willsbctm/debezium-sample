5e5f28954ce1d60923076df542bf86ba0313f7ce
William <willsbctm@gmail.com>
2024-10-29 11:04:44 -0300

    docs: Organize files

---
+++ pubsub/uninstall.ps1
@@ -0,0 +1,6 @@
+docker rm debezium-server
+docker rm debezium-sample-migrate-up-1
+docker rm debezium-sample-worker-1
+docker rm debezium-sample-api-1
+docker rm debezium-sample-pubsub-1
+docker rm debezium-sample-database-1
